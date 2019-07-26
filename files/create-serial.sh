c3baeb6d92a1f417431a850d99a645b3b80825a1
Ulrik <ulrik.strid@outlook.com>
2019-07-26 11:31:19 +0200

    Try to quote the version

--- files/create-serial.sh
+++ files/create-serial.sh
@@ -1,8 +1,7 @@
 echo Version 2.4.6
-echo 2.4.6 > .serial
+echo '2.4.6' > .serial
 
 echo 'Reset date on .serial'
 touch -t 200905010101 .serial
 
-echo '.serial: ${cat .serial}'
-
+echo '.serial:' $(cat .serial)
